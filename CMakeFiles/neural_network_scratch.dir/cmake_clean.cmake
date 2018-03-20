5c287987ad6149ec5064d12f90f5fd2c81110813
Mohamed BEN HAMDOUNE <mbenhamd@localhost>
2018-03-20 20:02:10 +0100

    Improvment

---
+++ CMakeFiles/neural_network_scratch.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neural_network_scratch.dir/main.cpp.o"
+  "CMakeFiles/neural_network_scratch.dir/src/GenerateSampleTraining.cpp.o"
+  "CMakeFiles/neural_network_scratch.dir/src/Main.cpp.o"
+  "CMakeFiles/neural_network_scratch.dir/src/Net.cpp.o"
+  "CMakeFiles/neural_network_scratch.dir/src/Neuron.cpp.o"
+  "CMakeFiles/neural_network_scratch.dir/src/TrainingData.cpp.o"
+  "neural_network_scratch.pdb"
+  "neural_network_scratch"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neural_network_scratch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
